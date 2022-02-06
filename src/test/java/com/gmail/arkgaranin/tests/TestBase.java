bad3548183ff2138edab3f0eddb3d2bccf17d6d9
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-02-06 20:52:08 +0300

    Вернул название проперти browserVersion

--- src/test/java/com/gmail/arkgaranin/tests/TestBase.java
+++ src/test/java/com/gmail/arkgaranin/tests/TestBase.java
@@ -20,7 +20,7 @@ public class TestBase {
     Configuration.remote = "https://" + login + ":" + password + "@" + url;
 
     Configuration.browser = System.getProperty("browser");
-    Configuration.browserVersion = System.getProperty("version");
+    Configuration.browserVersion = System.getProperty("browserVersion");
     Configuration.browserSize = System.getProperty("browserSize");
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
