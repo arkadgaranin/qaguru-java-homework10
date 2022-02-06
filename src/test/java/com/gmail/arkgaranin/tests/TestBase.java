b144c39d151eea159586728357b0477cbacc1c67
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-02-06 15:49:05 +0300

    Сконфигурировал браузер и удаленный запуск с getProperty

--- src/test/java/com/gmail/arkgaranin/tests/TestBase.java
+++ src/test/java/com/gmail/arkgaranin/tests/TestBase.java
@@ -2,6 +2,7 @@ package com.gmail.arkgaranin.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.gmail.arkgaranin.helpers.Attachments;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,9 +10,21 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class TestBase {
 
   @BeforeAll
+  @Step("Конфигурируем браузер и удаленный запуск")
   static void setup() {
+    String browser = System.getProperty("browser");
+    String browserVersion = System.getProperty("browserVersion");
+    String browserSize = System.getProperty("browserSize");
+    String url = System.getProperty("url");
+    String login = System.getProperty("login");
+    String password = System.getProperty("password");
+    String remoteUrl = "https://" + login + ":" + password + "@" + url;
+
+    Configuration.browser = browser;
+    Configuration.browserVersion = browserVersion;
+    Configuration.browserSize = browserSize;
+    Configuration.remote = remoteUrl;
     Configuration.baseUrl = "https://demoqa.com";
-//    Configuration.browserSize = "1920x1080";
 //    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
