4fe229407a2e544eb76be0a392a03ce89d55d6ab
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-02-06 13:17:50 +0300

    Скопировал директорию src c ДЗ №9 для доработки

---
+++ src/test/java/com/gmail/arkgaranin/tests/TestBase.java
@@ -0,0 +1,30 @@
+package com.gmail.arkgaranin.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.gmail.arkgaranin.helpers.Attachments;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+
+  @BeforeAll
+  static void setup() {
+    Configuration.baseUrl = "https://demoqa.com";
+    Configuration.browserSize = "1920x1080";
+    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("enableVNC", true);
+    capabilities.setCapability("enableVideo", true);
+    Configuration.browserCapabilities = capabilities;
+  }
+
+  @AfterEach
+  void addAttachments() {
+    Attachments.takeScreenshot();
+    Attachments.takePageSource();
+    Attachments.addVideo();
+    Attachments.browserConsoleLogs();
+  }
+}
