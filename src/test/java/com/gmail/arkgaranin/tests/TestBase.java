7d3ad1060a24e15aebe17062a77bc517f9491926
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-02-06 19:18:17 +0300

    Снова отредактировал конфигурацию

--- src/test/java/com/gmail/arkgaranin/tests/TestBase.java
+++ src/test/java/com/gmail/arkgaranin/tests/TestBase.java
@@ -12,16 +12,16 @@ public class TestBase {
   @BeforeAll
 //  @Step("Конфигурируем браузер и удаленный запуск")
   static void setup() {
-//    Configuration.baseUrl = "https://demoqa.com";
-    Configuration.browser = System.getProperty("browser", "chrome");
-    Configuration.browserVersion = System.getProperty("browserVersion", "97");
-    Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+    Configuration.baseUrl = "https://demoqa.com";
 
     String login = System.getProperty("login");
     String password = System.getProperty("password");
     String url = System.getProperty("url");
-    String remoteUrl = "https://" + login + ":" + password + "@" + url;
-    Configuration.remote = remoteUrl;
+    Configuration.remote = "https://" + login + ":" + password + "@" + url;
+
+    Configuration.browser = System.getProperty("browser");
+    Configuration.browserVersion = System.getProperty("browserVersion");
+    Configuration.browserSize = System.getProperty("browserSize");
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability("enableVNC", true);
