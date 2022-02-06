892be2d288304964d0c92ce79bcd6f5e1991c0a4
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-02-06 14:22:04 +0300

    Закомментил разрешение браузера и remoteUrl, т.к настроил их в jenkins

--- src/test/java/com/gmail/arkgaranin/tests/TestBase.java
+++ src/test/java/com/gmail/arkgaranin/tests/TestBase.java
@@ -11,8 +11,8 @@ public class TestBase {
   @BeforeAll
   static void setup() {
     Configuration.baseUrl = "https://demoqa.com";
-    Configuration.browserSize = "1920x1080";
-    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//    Configuration.browserSize = "1920x1080";
+//    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability("enableVNC", true);
